cdc2d40584b1915882ef3e90b7da6eb1c284c0db
davidhickey <davidhickey423@gmail.com>
2018-08-21 14:26:04 -0400

    active admin views rendering successfully

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,6 +35,10 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+#default url option
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3001 }
+
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
