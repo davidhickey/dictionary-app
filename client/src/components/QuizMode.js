4d2b1a516f7ae6cc3659cb3b479ccf4b1e8de35b
davidhickey <davidhickey423@gmail.com>
2018-12-14 09:50:19 -0500

    updates branch

--- client/src/components/QuizMode.js
+++ client/src/components/QuizMode.js
@@ -4,6 +4,61 @@ import { Container, Header, Segment, Form, Input, TextArea, Button, Icon, Dimmer
 
 class QuizMode extends Component {
 
+ state = {
+     index: 0,
+     showQuestion: true,
+     correctCounter: 0,
+     incorrectCounter: 0
+ }
+
+ // onCorrect = this.onCorrect.bind(this)
+ // onIncorrect = this.onIncorrect.bind(this)
+
+ onCorrect = () => {
+       this.setState((state) => ({
+           index: state.index + 1,
+           correctCounter: state.correctCounter + 1
+       }))
+   }
+
+   onIncorrect = () => {
+       this.setState((state) => ({
+           index: state.index + 1,
+           incorrectCounter: state.correctCounter + 1
+       }))
+   }
+
+   componentDidMount () {
+     this.getQuizCards()
+   }
+
+   fetch (endpoint) {
+     return window.fetch(endpoint)
+       .then(response => response.json())
+       .catch(error => console.log(error))
+   }
+
+   getQuizCards () {
+     this.fetch('/api/cards')
+       .then(cards => {
+         if (cards.length) {
+          var randomCard = cards[Math.floor(Math.random()*cards.length)]
+           this.getQuizCard(cards[randomCard].id)
+           console.log('got cards from api');
+         } else {
+           // this.setState({cards: []})
+         }
+       })
+   }
+
+   getQuizCard (id) {
+     this.fetch(`/api/cards/${id}`)
+       // .then(card => this.setState({card: card, quiz_id: this.props.quiz_id}))
+   }
+
+
+
+
 
 
   //gets all Cards
@@ -12,7 +67,7 @@ class QuizMode extends Component {
   //asks if I got it right or wrong
   //shows total count of correct and wrong words for quiz
   //button for reset quiz
-  
+
 
   render () {
     return (
